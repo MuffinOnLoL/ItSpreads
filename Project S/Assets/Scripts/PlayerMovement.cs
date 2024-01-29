cf561bbfbedcf0e41bb0f17a7ed0bfbe8923c3b9
Muffin <colink7112@yahoo.com>
2024-01-29 14:50:04 -0600

    Creating Initial Objects/folders

---
+++ Project S/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+//PlayerMovement.cs
+//Colin Klein
+//1/29/2024
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+}
